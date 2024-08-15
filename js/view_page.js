c0423e3546c21434b16b31c8c6be465e875ec839
xuanbachlai <142763274+xuanbachlai@users.noreply.github.com>
2024-08-15 15:21:59 +0700

    Add files via upload

--- js/view_page.js
+++ js/view_page.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleSwitch = document.getElementById('toggle-switch');
     const viewPage2Elements = document.querySelectorAll('.ViewPage2');
+    const viewPage1Elements = document.querySelectorAll('.ViewPage1');
 
     function updateView() {
         if (toggleSwitch.checked) {
             viewPage2Elements.forEach(el => el.style.display = 'block');
+            viewPage1Elements.forEach(el => el.style.display = 'none');
         } else {
             viewPage2Elements.forEach(el => el.style.display = 'none');
+            viewPage1Elements.forEach(el => el.style.display = 'block');
         }
     }
 
     updateView();
+
     toggleSwitch.addEventListener('change', updateView);
 });
